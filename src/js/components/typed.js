e8bdd1715349f4fbdc18282ba1358d3e6e022014
khaisendefo <khaisendefo@gmail.com>
2024-07-31 20:51:39 +0300

    Added and configured typed.js library

---
+++ src/js/components/typed.js
@@ -0,0 +1,16 @@
+import Typed from 'typed.js';
+
+export const initializeTyped = () => {
+  new Typed('.typed-text', {
+    strings: [
+      'Разработка <span>уникального сайта</span> для вашего бизнеса',
+      '<span>Кроссбраузерная верстка</span> по макету figma',
+      'Разработка сайта от <span>концепции до запуска</span>',
+    ],
+    typeSpeed: 50,
+    loop: true,
+    backSpeed: 25,
+    backDelay: 1000,
+    loopCount: Infinity,
+  });
+};
--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,9 @@ import '/scss/main.scss';
 // import components
 import { useToggle } from './components/toggle.js';
 import { useSidebarMenu } from './components/home/accordion-sidebar.js';
+import { initializeTyped } from './components/typed.js';
 
 // function call
 useToggle();
 useSidebarMenu();
+initializeTyped();
